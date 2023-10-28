220205cec2cfe61cbee745015f41b71d1c067f72
Muhammad Zaka Ullah <97341177+Zaka112@users.noreply.github.com>
2023-10-28 23:57:16 +0200

    pagination

--- src/pages/products/ProductList.tsx
+++ src/pages/products/ProductList.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
-import { CircularProgress, Grid, Paper, Typography } from "@mui/material";
+import { CircularProgress, Grid, Pagination, Paper, Typography } from "@mui/material";
 
 import { AppDispatch, RootState } from "../../redux/store";
 import { getProductsData } from "../../redux/thunk/products";
@@ -10,19 +10,31 @@ import SearchForm from "../../components/SearchForm";
 import SortProducts from "../../components/SortProducts";
 
 export default function ProductList() {
+  const [currentPage, setCurrentPage] = React.useState(1);
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     dispatch(getProductsData());
   }, [dispatch]);
 
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    page: number
+  ) => {
+    setCurrentPage(page);
+  };
   const products = useSelector((state: RootState) => state.products.products);
+  const itemsPerPage = 28;
+  const totalPages = Math.ceil(products.length / itemsPerPage);
   const isLoading = useSelector((state: RootState) => state.products.isLoading);
   const searchedText = useSelector(
     (state: RootState) => state.search.searchedString
   );
 
-  const searchedProduct = products.filter((product) =>
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const productsPerPage = products.slice(startIndex, endIndex);
+  const searchedProduct = productsPerPage.filter((product) =>
     product.title.toLowerCase().includes(searchedText.toLowerCase())
   );
 
@@ -59,7 +71,15 @@ export default function ProductList() {
             return <ProductItem product={product} key={product.id} />;
           })}
         </Grid>
+        
       )}
+      <Pagination
+          count={totalPages}
+          page={currentPage}
+          onChange={handlePageChange}
+        />
     </Paper>
+    
   );
+  
 }
